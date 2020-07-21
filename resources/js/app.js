5651d77f5566a1e930c26a731497a1fd14df1ee6
ShahadatPOM <naiem.3112@gmail.com>
2020-07-22 00:42:22 +0600

    iviewui installed and admin template layout ready

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,12 @@ window.Vue = require('vue');//load vue
 
 Vue.component('mainapp', require('./components/mainapp').default);
 
+//iviewui
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+Vue.use(ViewUI);
+
+
 // import router
 import router from './router.js'//understanding problem
 
