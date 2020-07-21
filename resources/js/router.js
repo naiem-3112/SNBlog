8f9aebe3a58f7dbe2ce9100743a4837fdc45ec56
ShahadatPOM <naiem.3112@gmail.com>
2020-07-22 00:56:38 +0600

    admin home page coded initially

--- resources/js/router.js
+++ resources/js/router.js
@@ -5,8 +5,18 @@ import firstPage from './components/pages/firstpage'
 import two from './components/pages/two'
 import hook from './components/pages/hook'
 import method from './components/pages/basicmethod'
+import home from './components/project/home'
 
 const routes = [
+
+    // project
+    {
+        path: '/home',
+        component: home,
+    },
+
+
+
     {
         path: '/one',
         component: firstPage,
